6070e43413aee8ed12666351fae5593dd81fea6d
Alibek <alibekabdrakhman0@gmail.com>
2022-06-13 16:46:58 +0600

    hw

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import firstImage from "./images/first.jpeg"
+import secondImage from "./images/second.jpg"
+import thirdImage from "./images/third.jpeg"
+import fourthImage from "./images/fourth.jpeg"
+import fifthImage from "./images/fifth.jpeg"
+import "./App.css"
+import logo from "./logo.png";
+import {format} from 'date-fns'
 
+const date = format(new Date(), "dd.MM.yyyy HH:mm");
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <div className="input">
+          <img src={logo} className="logo"/>
+          <input></input>
+
+        </div>
+        <div className="date">
+          {date}
+        </div>
+      </div>
+      <div className="main">
+        <div className="parent">
+          <img src={firstImage} className="firstImage"/>
+          <h1 className="text">Hey</h1>
+        </div>
+        <div className="parent">
+          <img src={secondImage} className="secondImage"/>
+          <h1 className="text">Let's</h1>
+        </div>
+        <div className="parent">
+          <img src={thirdImage} className="thirdImage"/>
+          <h1 className="text">Give</h1>
+        </div>
+        <div className="parent">
+          <img src={fourthImage} className="fourthImage"/>
+          <h1 className="text">All</h1>
+        </div>
+        <div className="parent">
+          <img src={fifthImage} className="fifthImage"/>
+          <h1 className="text">You can</h1>
+        </div>
+      </div>
+
+    </>
+      
+
   );
 }
 
